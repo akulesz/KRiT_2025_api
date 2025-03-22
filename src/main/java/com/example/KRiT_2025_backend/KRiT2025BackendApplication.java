0d95d6a4359f0f5e4f2c054e7430aeb880835500
agatherat <kulesz.agnieszka@wp.pl>
2025-03-22 14:07:08 +0100

    initialize project

---
+++ src/main/java/com/example/KRiT_2025_backend/KRiT2025BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.KRiT_2025_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KRiT2025BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KRiT2025BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/KRiT_2025_backend/KRiT2025BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KRiT_2025_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KRiT2025BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
